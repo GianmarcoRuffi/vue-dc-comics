2554177e57cf21bae2060c20608f99d1a710f849
GianmarcoRuffi <gianmarco.ruffi@outlook.it>
2022-05-04 20:05:20 +0200

    Fix bottom footer

--- src/components/BottomSocials.vue
+++ src/components/BottomSocials.vue
@@ -4,14 +4,25 @@
     id="socials"
     class="container d-flex justify-content-around align-items-center"
   >
-    <div class="btn-signup">Sign up</div>
+    <div class="btn-signup"><span>Sign-Up NOW!</span></div>
     <div class="links">
       <ul>
         <li><a href="">Follow us</a></li>
-        <li><a href="">fa</a></li>
-        <li><a href="">fa</a></li>
-        <li><a href="">fa</a></li>
-        <li><a href="">fa</a></li>
+        <li>
+          <a href=""><i class="fa-brands fa-facebook-f"></i></a>
+        </li>
+        <li>
+          <a href=""><i class="fa-brands fa-twitter"></i></a>
+        </li>
+        <li>
+          <a href=""><i class="fa-brands fa-youtube"></i></a>
+        </li>
+        <li>
+          <a href=""><i class="fa-brands fa-pinterest-p"></i></a>
+        </li>
+        <li>
+          <a href=""><i class="fa-solid fa-location-dot"></i></a>
+        </li>
       </ul>
     </div>
   </div>
