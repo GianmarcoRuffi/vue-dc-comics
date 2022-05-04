9bab4b02c6759dcfe4621540b58b3d87660e3168
GianmarcoRuffi <gianmarco.ruffi@outlook.it>
2022-05-04 23:36:56 +0200

    Terzo fix

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,54 +1,54 @@
 !
 <template>
   <footer>
-    <div class="container">
-      <div class="footer-left d-flex">
-        <div class="row">
-          <div class="col-4">
-            <h2>DC Comics</h2>
-            <ul>
-              <li><a href="">Characters</a></li>
-              <li><a href="">Comics</a></li>
-              <li><a href="">Movies</a></li>
-              <li><a href="">TV</a></li>
-              <li><a href="">Games</a></li>
-              <li><a href="">Videos</a></li>
-              <li><a href="">News</a></li>
-            </ul>
-            <h2>Shop</h2>
-            <ul>
-              <li><a href="">Shop DC</a></li>
-              <li><a href="">Shop DC Collectibles</a></li>
-            </ul>
-          </div>
-          <div class="col-4">
-            <h2>DC</h2>
-            <ul>
-              <li><a href="">Terms Of Use</a></li>
-              <li><a href="">Privacy Policy</a></li>
-              <li><a href="">Ad Choices</a></li>
-              <li><a href="">Advertising</a></li>
-              <li><a href="">Jobs</a></li>
-              <li><a href="">Subscriptions</a></li>
-              <li><a href="">CPSC Certificates</a></li>
-              <li><a href="">Ratings</a></li>
-              <li><a href="">Shop Help</a></li>
-              <li><a href="">Contact Us</a></li>
-            </ul>
-          </div>
-          <div class="col-4">
-            <h2>Sites</h2>
-            <ul>
-              <li><a href="">DC</a></li>
-              <li><a href="">MAD Magazine</a></li>
-              <li><a href="">DC Kids</a></li>
-              <li><a href="">DC Universe</a></li>
-              <li><a href="">DC Power Visa</a></li>
-            </ul>
-          </div>
+    <div class="footer-left d-flex">
+      <div class="row d-flex">
+        <div class="col-4">
+          <h2>DC Comics</h2>
+          <ul>
+            <li><a href="">Characters</a></li>
+            <li><a href="">Comics</a></li>
+            <li><a href="">Movies</a></li>
+            <li><a href="">TV</a></li>
+            <li><a href="">Games</a></li>
+            <li><a href="">Videos</a></li>
+            <li><a href="">News</a></li>
+          </ul>
+          <h2>Shop</h2>
+          <ul>
+            <li><a href="">Shop DC</a></li>
+            <li><a href="">Shop DC Collectibles</a></li>
+          </ul>
+        </div>
+        <div class="col-4">
+          <h2>DC</h2>
+          <ul>
+            <li><a href="">Terms Of Use</a></li>
+            <li><a href="">Privacy Policy</a></li>
+            <li><a href="">Ad Choices</a></li>
+            <li><a href="">Advertising</a></li>
+            <li><a href="">Jobs</a></li>
+            <li><a href="">Subscriptions</a></li>
+            <li><a href="">CPSC Certificates</a></li>
+            <li><a href="">Ratings</a></li>
+            <li><a href="">Shop Help</a></li>
+            <li><a href="">Contact Us</a></li>
+          </ul>
+        </div>
+        <div class="col-4">
+          <h2>Sites</h2>
+          <ul>
+            <li><a href="">DC</a></li>
+            <li><a href="">MAD Magazine</a></li>
+            <li><a href="">DC Kids</a></li>
+            <li><a href="">DC Universe</a></li>
+            <li><a href="">DC Power Visa</a></li>
+          </ul>
         </div>
       </div>
-      <div class="footer-right"></div>
+    </div>
+    <div class="footer-right">
+      <img src="../assets/img/dc-logo-bg.png" alt="" />
     </div>
   </footer>
 </template>
