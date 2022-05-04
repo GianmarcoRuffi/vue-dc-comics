5427b3248fd745d597810228377953639a2bce8b
GianmarcoRuffi <gianmarco.ruffi@outlook.it>
2022-05-04 18:11:14 +0200

    Content

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,12 +1,19 @@
 !
 <template>
-  <header><div class="hello"></div></header>
+  <body>
+    <div class="main">
+      <div class="row placeholder">
+        <div class="col-6"><h1>-->Content goes here >--</h1></div>
+      </div>
+    </div>
+  </body>
 </template>
 
 <script>
-export default { name: "Header" };
+export default { name: "MainContent" };
 </script>
 
 <style scoped lang="scss">
 @import "@/assets/maincontent.scss";
+// @import "@/assets/generals.scss";
 </style>
